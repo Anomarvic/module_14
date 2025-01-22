f75888d6117e0d52a365953ad5e75e5b06e54abe
Anomarvic <anomarvic@gmail.com>
2025-01-22 12:57:19 +0300

    new

---
+++ module_14_3.py
@@ -0,0 +1,96 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import asyncio
+
+api = ''
+bot = Bot(token= api)
+dp = Dispatcher(bot, storage= MemoryStorage())
+
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+button = KeyboardButton(text='Рассчитать')
+button2 = KeyboardButton(text='Информация')
+button3 = KeyboardButton(text='Купить')
+kb.row(button, button2)
+kb.add(button3)
+
+catalog_kb = InlineKeyboardMarkup(resize_keyboard=True)
+button4 = InlineKeyboardButton(text= 'Продукт 1', callback_data= 'product_buying')
+button5 = InlineKeyboardButton(text= 'Продукт 2', callback_data= 'product_buying')
+button6 = InlineKeyboardButton(text= 'Продукт 3', callback_data= 'product_buying')
+button7 = InlineKeyboardButton(text= 'Продукт 4', callback_data= 'product_buying')
+catalog_kb.row(button4, button5, button6, button7)
+
+# buy_kb = InlineKeyboardMarkup(
+#     inline_keyboard=[
+#         [InlineKeyboardButton(text= 'Купить', callback_data= 'product_buying')],
+#         [InlineKeyboardButton(text= 'Назад', callback_data= 'back_to_catalog')]
+#     ]
+# )
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+@dp.message_handler(commands=['start'])
+async def start_message(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.', reply_markup=kb)
+
+@dp.message_handler(text=['Рассчитать'])
+async def set_age(message):
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+@dp.message_handler(state= UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age = message.text)
+    await message.answer('Введите свой рост:')
+    await UserState.growth.set()
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth = message.text)
+    await message.answer('Введите свой вес:')
+    await UserState.weight.set()
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight = message.text)
+    data = await state.get_data()
+
+    age = int(data.get('age'))
+    growth = int(data.get('growth'))
+    weight = int(data.get('weight'))
+
+    calories = 10 * weight + 6.25 * growth - 5 * age - 161
+
+    await message.answer(f'Ваша норма калорий: {calories}')
+    await state.finish()
+
+@dp.message_handler(text=['Информация'])
+async def inform(message):
+    await message.answer('Информация')
+
+@dp.message_handler(text=['Купить'])
+async def get_buying_list(message):
+    products = []
+    for i in range(1, 5):
+        products.append(f'Название: Продукт {i}| Описание: описание {i} | Цена: {i * 100}')
+
+        for product in products:
+            with open(f'files/{i}.png', 'rb') as img:
+                await message.answer_photo(img, product)
+
+    await message.answer("Выберите продукт для покупки:", reply_markup=catalog_kb)
+
+@dp.callback_query_handler(text='product_buying')
+async def send_confirm_message(call):
+    await call.answer()
+    await call.message.answer("Вы успешно приобрели продукт!")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates= True)
